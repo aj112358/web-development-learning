ed33eff573c3e76445cbcaed1c104c7ca7b42766
AJ <amarjits1@hotmail.com>
2022-07-27 01:33:23 -0600

    feat: incorporating event listener to keyboard events

--- advanced_js_and_dom/index.js
+++ advanced_js_and_dom/index.js
@@ -1,13 +1,55 @@
 function handleClick() {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-    console.log(this);
-    // this.setAttribute("style", "color: white;");
-    this.style.color = "white";
+    var imageLetter = this.textContent;
+    playSound(imageLetter);
 }
 
-
 var buttons = document.querySelectorAll(".drum");
-for (var i=0; i<buttons.length; i++) {
+for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", handleClick);
+}
+
+document.addEventListener("keypress", function(event) {
+    playSound(event.key);
+});
+
+function playSound(letter) {
+    switch (letter) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        
+        case "a":
+            var kickBase = new Audio("sounds/kick-bass.mp3");
+            kickBase.play();
+            break;
+
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default:
+            console.log(letter);
+    }
 }
