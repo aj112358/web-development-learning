58edccd8f94cc94cd8ffafe709bc92a84df75c7a
AJ <amarjits1@hotmail.com>
2022-07-28 00:51:27 -0600

    feat: finished learning how to use jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,39 @@
-$(document).ready(function() {$("button").css("color", "blue");});
+// $(document).ready(function() {$("button").css("color", "blue");});
 
-$("h1").css("color", "red");
+// $("h1").css("color", "red");
 
+
+$("button").css("color", "green");
+
+$("h1").addClass("big-title margin-50");
+
+// $("button").text("sup");
+// $("button").html("<em>CLICK</em>");
+
+$("h1").click(function() {
+    $("h1").css("color", "blue");
+});
+
+
+$("button").click(function() {
+    $("h1").css("color", "red");
+});
+
+$("input").keypress(function(event) {
+    console.log(event.key);
+    $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "black");
+});
+
+$("h1").before("<button>New</button>")
+
+$(".btn").on("click", function() {
+    $("h1").toggle();
+});
+
+$(".transparent").on("click", function() {
+    $("h1").animate({opacity: 0.3});
+});
